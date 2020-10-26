085afcfa3e012590b5ddeafaadd193cbefe9c13f
lisa6125 <fxck6125@gmail.com>
2020-10-26 11:20:48 +0800

    調整完畢

--- index.js
+++ index.js
@@ -30,74 +30,68 @@ const bot = linebot({
 
 bot.on('message', async (event) => {
   try {
-    let reply
+    let reply = ''
+    let reply2
     const text = event.message.text
     if (text === 'flex') {
-      reply = {
+      reply2 = {
         type: 'flex',
         altText: 'Flex',
-        contents: [{
-          type: 'bubble',
-          hero: {
-            type: 'image',
-            url: 'https://cc.tvbs.com.tw/img/program/_data/i/upload/2017/04/21/20170421152859-3c494080-me.jpg',
-            size: 'full',
-            aspectRatio: '20:13',
-            aspectMode: 'cover',
-            action: {
-              type: 'uri',
-              uri: 'http://linecorp.com/'
+        contents: {
+          type: 'carousel',
+          contents: [{
+            type: 'bubble',
+            hero: {
+              type: 'image',
+              url: 'https://cc.tvbs.com.tw/img/program/_data/i/upload/2017/04/21/20170421152859-3c494080-me.jpg',
+              size: 'full',
+              aspectRatio: '20:13',
+              aspectMode: 'cover',
+              action: {
+                type: 'uri',
+                uri: 'https://www.twfood.cc/'
+              }
+            },
+            body: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'text',
+                  text: '蔬果查價',
+                  weight: 'bold',
+                  size: 'xl'
+                }
+              ],
+              backgroundColor: '#E9F5DF'
+            },
+            footer: {
+              type: 'box',
+              layout: 'vertical',
+              spacing: 'sm',
+              contents: [
+                {
+                  type: 'button',
+                  style: 'link',
+                  height: 'sm',
+                  action: {
+                    type: 'uri',
+                    label: 'WEBSITE',
+                    uri: 'https://www.twfood.cc/'
+                  },
+                  color: '#5D8C54'
+                },
+                {
+                  type: 'spacer',
+                  size: 'sm'
+                }
+              ],
+              flex: 0
             }
-          },
-          body: {
-            type: 'box',
-            layout: 'vertical',
-            contents: [
-              {
-                type: 'text',
-                text: '蔬果查價',
-                weight: 'bold',
-                size: 'xl'
-              }
-            ],
-            backgroundColor: '#E9F5DF'
-          },
-          footer: {
-            type: 'box',
-            layout: 'vertical',
-            spacing: 'sm',
-            contents: [
-              {
-                type: 'button',
-                style: 'link',
-                height: 'sm',
-                action: {
-                  type: 'message',
-                  label: '直接輸入菜名',
-                  text: 'EX:白菜、西瓜......'
-                },
-                color: '#5D8C54'
-              },
-              {
-                type: 'button',
-                style: 'link',
-                height: 'sm',
-                action: {
-                  type: 'uri',
-                  label: 'WEBSITE',
-                  uri: 'https://www.twfood.cc/'
-                },
-                color: '#5D8C54'
-              },
-              {
-                type: 'spacer',
-                size: 'sm'
-              }
-            ],
-            flex: 0
-          }
-        }]
+          }]
+        }
       }
+      event.reply(reply2)
     } else {
       for (const inform of informations) {
         if (inform.作物名稱.includes(text)) {
@@ -105,8 +99,8 @@ bot.on('message', async (event) => {
         }
       }
       reply = (reply.length === 0) ? '找不到呦~~~' : reply
+      event.reply(reply)
     }
-    event.reply(reply)
   } catch (error) {
     event.reply('發生錯誤')
   }
